e859b5a6d61e037f45289743796fe47bfb0ef679
ivoobraznaa <ivoobraznaa@gmail.com>
2025-05-11 20:03:20 +0300

    First Tests

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class FirstTest extends BaseTest {
+
+    @Test
+    public void zipCode4DigitsCheck() {
+        // 1. открыть браузер
+        // 2. Зайти на сайт
+        browser.get("https://sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("1234");
+        browser.findElement(By.xpath("//*[@value='Continue']")).click();
+        assertEquals(browser.findElement(By.cssSelector(".error_message")).getText(), "Oops, error on page. ZIP code should have 5 digits", "ZIP code should have 5 digits");
+    }
+
+    @Test
+    public void zipCode5DigitsCheck() {
+        browser.get("https://sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("12345");
+        browser.findElement(By.xpath("//*[@value='Continue']")).click();
+        boolean regBtnIsPresent = browser.findElement(By.xpath("//*[@value='Register']")).isDisplayed();
+        assertTrue(regBtnIsPresent);
+    }
+}
